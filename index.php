c3c3604f00bfe53f77ebc89c4bf21c1206482d5c
Richard Peterson <webbuyrich@gmail.com>
2015-12-23 09:37:18 -0600

    Initial commit with hello

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Untitled</title>
+        
+        <link rel="author" href="humans.txt">
+        <script src="js/quickstart.js"></script>
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+     <?php include('includes/quickstart.php') ?>   
+        
+    </body>
+</html>
